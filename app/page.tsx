7bb87e4894c63b3fada5cb23be12f6f6f8ff8e7c
phongduong-dp <duonghongphong.dp@gmail.com>
2024-07-13 16:56:42 +0700

    chore: test tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <main>
-      <p>Hello, World</p>
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 sm:px-10">
+      <div className="w-full max-w-7xl">
+        <h1 className="text-4xl font-bold text-red-600">Phong Duong</h1>
+        <p className="text-2xl text-white">Software Engineer</p>
+      </div>
     </main>
   );
 }
