80d9183684ca2e7fe9ca2f9b40c368447df2c1ba
phongduong-dp <duonghongphong-dp@gmail.com>
2024-07-24 20:36:19 +0700

    chore: test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ export default function Home() {
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 sm:px-10">
       <div className="w-full max-w-7xl">
         <h1 className="text-4xl font-bold text-red-600">Phong Duong</h1>
-        <p className="text-2xl text-white">Software Engineer</p>
       </div>
     </main>
   );
