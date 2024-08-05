8f21eef5ed8f14535d4e6034f13b78124024715a
phongduong-dp <duonghongphong.dp@gmail.com>
2024-08-05 22:46:19 +0700

    feat: implement floating navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Hero from '@/components/Hero';
+import { FloatingNav } from '@/components/ui/FloatingNav';
+import { FaHome, FaUser, FaProjectDiagram, FaEnvelope } from 'react-icons/fa';
+
+const navItems = [
+  { name: 'Home', link: '/', icon: <FaHome /> },
+  { name: 'About', link: '/about', icon: <FaUser /> },
+  { name: 'Projects', link: '/projects', icon: <FaProjectDiagram /> },
+  { name: 'Contact', link: '/contact', icon: <FaEnvelope /> },
+];
 
 export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
       <div className="w-full max-w-7xl">
+        <FloatingNav navItems={navItems} />
         <Hero />
       </div>
     </main>
