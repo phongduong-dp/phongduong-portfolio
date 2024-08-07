c4c545404f737045f656671d1e2409b1e70c05a0
phongduong-dp <duonghongphong.dp@gmail.com>
2024-08-07 23:26:45 +0700

    chore: modify grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { BentoGrid, BentoGridItem } from './ui/BentoGrid';
 import { gridItems } from '@/data';
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid';
 
 const Grid = () => {
   return (
     <section id="about">
-      <BentoGrid>
-        {gridItems.map((item) => (
+      <BentoGrid className="w-full py-20">
+        {gridItems.map((item, i) => (
           <BentoGridItem
             id={item.id}
-            key={item.id}
+            key={i}
             title={item.title}
             description={item.description}
             className={item.className}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
         />
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      <div className="absolute left-0 top-0 flex h-screen w-full items-center justify-center bg-white bg-grid-black/[0.02] dark:bg-black-100 dark:bg-grid-white/[0.02]">
+      <div className="absolute left-0 top-0 flex h-screen w-full items-center justify-center bg-white bg-grid-black/[0.03] dark:bg-black-100 dark:bg-grid-white/[0.03]">
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
       </div>
       <div className="relative z-10 my-20 flex justify-center">
