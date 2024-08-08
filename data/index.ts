4f33af67b30ff3e74cec4188b346794d621bff63
phongduong-dp <duonghongphong-dp@gmail.com>
2024-08-08 21:38:15 +0700

    chore: change some data

--- data/index.ts
+++ data/index.ts
@@ -59,7 +59,7 @@ export const gridItems = [
   {
     id: 5,
     title: 'Currently building a JS Animation library',
-    description: 'The Inside Scoop',
+    description: 'Where am I',
     className: 'md:col-span-3 md:row-span-2',
     imgClassName: 'absolute right-0 bottom-0 md:w-96 w-60',
     titleClassName: 'justify-center md:justify-start lg:justify-center',
@@ -68,7 +68,7 @@ export const gridItems = [
   },
   {
     id: 6,
-    title: 'Do you want to start a project together?',
+    title: "Let's build something amazing together",
     description: '',
     className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
     imgClassName: '',
