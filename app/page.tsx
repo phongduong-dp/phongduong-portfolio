5dff5bc2cc5d86b9f4daa14e73259f7937909710
Phong Duong <92157056+phongduong-dp@users.noreply.github.com>
2024-08-12 22:15:29 +0700

    Feat/work experience (#12)
    * feat: add work experience section
    
    * fix: modify index data

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Experience from '@/components/Experience';
 import Grid from '@/components/Grid';
 import Hero from '@/components/Hero';
 import RecentProjects from '@/components/RecentProjects';
@@ -16,6 +17,7 @@ export default function Home() {
         <Grid />
         <TechStack />
         <RecentProjects />
+        <Experience />
       </div>
     </main>
   );
