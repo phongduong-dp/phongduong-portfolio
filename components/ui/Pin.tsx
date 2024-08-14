442e2eea436a9f7f4622d18806a8c91b58c32ee4
Phong Duong <92157056+phongduong-dp@users.noreply.github.com>
2024-08-14 22:44:40 +0700

    fix: add handleOnClick (#13)

--- components/ui/Pin.tsx
+++ components/ui/Pin.tsx
@@ -28,6 +28,10 @@ export const PinContainer = ({
     setTransform('translate(-50%,-50%) rotateX(0deg) scale(1)');
   };
 
+  const handleOnClick = () => {
+    window.open(href, '_blank');
+  };
+
   return (
     <div
       className={cn(
@@ -36,6 +40,7 @@ export const PinContainer = ({
       )}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
+      onClick={handleOnClick}
     >
       <div
         style={{
