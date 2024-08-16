ba2c2c78642c261996657cddbe2cfa441194fe1b
phongduong-dp <duonghongphong.dp@gmail.com>
2024-08-16 11:17:42 +0700

    fix: page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Approach from '@/components/Approach';
 import Experience from '@/components/Experience';
+import Footer from '@/components/Footer';
 import Grid from '@/components/Grid';
 import Hero from '@/components/Hero';
 import RecentProjects from '@/components/RecentProjects';
@@ -11,7 +12,7 @@ import { navItems } from '@/data';
 
 export default function Home() {
   return (
-    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-clip bg-black-100 px-5 sm:px-10">
       <div className="w-full max-w-7xl">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -20,6 +21,7 @@ export default function Home() {
         <RecentProjects />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
